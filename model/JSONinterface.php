855078ac45d7f84608ef2ad16c223e77f3f2291d
goran <gboban70@gmail.com>
2015-02-02 14:28:36 +0100

    added license information, fixed require_once in WSInterface.php

--- model/JSONinterface.php
+++ model/JSONinterface.php
@@ -1,9 +1,25 @@
 <?php
-/*
- * NOTEs:
- * 	- annotations are for php2wsdl if required
+/**
+ * @author goran
+ * @filesource
+ *
+ * JSONServer - handles JSON requests and dispatches them to appropriete objects
+ * Enforces RESTful design
+ *
  */
-require_once("model/wsimplementation.php");
+/**
+ * This program is free software: you can redistribute it and/or modify
+ it under the terms of the GNU General Public License as published by
+ the Free Software Foundation, either version 3 of the License, or
+ (at your option) any later version.
+ This program is distributed in the hope that it will be useful,
+ but WITHOUT ANY WARRANTY; without even the implied warranty of
+ MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+ GNU General Public License for more details.
+ You should have received a copy of the GNU General Public License
+ along with this program.  If not, see <http://www.gnu.org/licenses/>.
+ */
+require_once("Contact.php");
 
 class JSONInterface{
 	private $_impl;
